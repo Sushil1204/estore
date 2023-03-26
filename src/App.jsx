b819b592ca46bdaab6e206df6ee1972c783bde5f
Sushil1204 <sushilspundkar@gmail.com>
2023-03-26 13:52:24 +0530

    Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import ProductsList from "./components/ProductsList";
+import Product from "./components/Product";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./components/Cart";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductsList/>} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route>404 Not Found!</Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
